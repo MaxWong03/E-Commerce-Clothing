d042926616f1bb7dac053e50ca474ba4964e7398
Max Wong <max@techsamurais.com>
2019-12-09 15:47:36 -0800

    build skeleton code for shoppage and preview collection

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import './App.css';
-import Homepage from './pages/HomePage/Homepage';
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>
-        Hats Page
-      </h1>
-    </div>
-  )
-}
+import Homepage from './pages/HomePage/Homepage';
+import ShopPage from './pages/ShopPage/Shoppage';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={Homepage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
--- src/pages/ShopPage/Shoppage.js
+++ src/pages/ShopPage/Shoppage.js
@@ -2,14 +2,20 @@ import React, { useState } from 'react';
 
 import './Shoppage.scss';
 
-import SHOP_DATA  from './collections';
+import SHOP_DATA from './collections';
+
+import PreviewCollection from '../../components/Preview-Collection/PreviewCollection';
 
 const ShopPage = () => {
   const [collections, setCollection] = useState(SHOP_DATA);
 
-  return(
-    <div>
-      Shop page
+  return (
+    <div className='shop-page'>
+      {
+        collections.map(({ id, ...collectionProps }) => (
+          <PreviewCollection key={id} {...collectionProps} />
+        ))
+      }
     </div>
   )
 }
