3837531936f1bd1b81f32c6bd0d4113b16ca90a9
Max Wong <max@techsamurais.com>
2019-12-09 10:43:24 -0800

    install sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Homepage from './Homepage';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Homepage />
   );
 }
 
