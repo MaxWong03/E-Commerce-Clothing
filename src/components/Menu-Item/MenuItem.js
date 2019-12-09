d6e30e889ca986db8814af507476f1406e8e5171
Max Wong <max@techsamurais.com>
2019-12-09 11:38:28 -0800

    adjust images for menu item

--- src/components/Menu-Item/MenuItem.js
+++ src/components/Menu-Item/MenuItem.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 import './MenuItem.scss';
 
-const MenuItem = ({ title }) => {
+import classNames from 'classnames';
+
+const MenuItem = ({ title, imageUrl, size }) => {
+
+  const menuItem = classNames('menu-item', {
+    'menu-item large': size
+  });
+
   return (
-    <div className='menu-item'>
+    <div style={{
+      backgroundImage: `url(${imageUrl})`
+    }} className={menuItem}>
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
