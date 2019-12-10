5ed7bf28f2b8e1968b7f591058a6886ac833bb3e
Max Wong <max@techsamurais.com>
2019-12-10 10:36:47 -0800

    create template for sign in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 import Homepage from './pages/HomePage/Homepage';
 import ShopPage from './pages/ShopPage/Shoppage';
+import SignInSignUp from './pages/Sign-in-and-sign-up/SignInAndSignUp';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/shop' component={ShopPage} />
+      {/* <Switch> */}
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp} />
+      {/* </Switch> */}
     </div>
   );
 }
---
+++ src/pages/Sign-in-and-sign-up/SignInAndSignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SignInAndSignUp.scss';
+
+const SignInAndSignUp = () => {
+  return(
+    <div>
+      Sign In
+    </div>
+  )
+}
+
+export default SignInAndSignUp;
