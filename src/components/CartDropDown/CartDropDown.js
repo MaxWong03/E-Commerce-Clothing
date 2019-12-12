76c0fd991bf7745b68732c3aadf60b6097563523
Max Wong <max@techsamurais.com>
2019-12-12 14:54:18 -0800

    display added item in cart dropdown

--- src/components/CartDropDown/CartDropDown.js
+++ src/components/CartDropDown/CartDropDown.js
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import { useSelector, shallowEqual } from 'react-redux';
 import CustomButton from '../CustomButton/CustomButton';
+import CartItem from '../CartItem/CartItem';
 
 import './CartDropDown.scss';
 
 const CartDropDown = () => {
+  const { cartItems } = useSelector(state => state.cart, shallowEqual);
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items'/>
+      <div className='cart-items'>
+        {
+          cartItems.map(cartItem => (
+            <CartItem 
+              key={cartItem.id}
+              item={cartItem}
+            />
+          ))
+        }
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   )
