f6a22edd370c94e0804d21f3183b19a53423bc9e
Max Wong <max@techsamurais.com>
2019-12-13 12:10:17 -0800

    style checkout page and total

--- src/pages/CheckOutPage/CheckOutPage.js
+++ src/pages/CheckOutPage/CheckOutPage.js
@@ -1,13 +1,42 @@
-import React from 'react';
-
+import React, { memo } from 'react';
+import { useSelector } from 'react-redux';
+import isEqual from 'lodash/isEqual';
 import './CheckOutPage.scss';
 
 const CheckOutPage = () => {
+  const cartItems = useSelector(state => state.cart.cartItems, isEqual);
+  const calTotal = cartItemsArr => {
+    return cartItemsArr.reduce((accumlator, cartItem) =>
+      accumlator + cartItem.quantity * cartItem.price
+      , 0);
+  }
   return (
-    <div>
-      CHECK OUT PAGE
+    <div className='checkout-page'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+      {
+        cartItems.map(cartItem => cartItem.name)
+      }
+      <div className='total'>
+        <span>Total: ${calTotal(cartItems)}</span>
+      </div>
     </div>
   )
 }
 
-export default CheckOutPage;
+export default memo(CheckOutPage);
