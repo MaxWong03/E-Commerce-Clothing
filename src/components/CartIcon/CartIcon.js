43322cf481117c6270b9994a0ab6efbf9e049787
Max Wong <max@techsamurais.com>
2019-12-18 11:58:18 -0800

    rewrite carticon using reselect to improve performance

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,24 +1,18 @@
 import React, { memo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { selectCartItemCount } from '../../redux/cart/cart.selector';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import isEqual from 'lodash/isEqual';
 import './CartIcon.scss';
 
 const CartIcon = memo(() => {
   const dispatch = useDispatch();
-  const cartItems = useSelector(state => state.cart.cartItems, isEqual);
-  const getItemNumber = cartItemsArr => {
-    return cartItemsArr.reduce(
-      (accumlator, cartItem) => accumlator + cartItem.quantity,
-      0
-    )
-  }
+  const cartItemsCount = useSelector(selectCartItemCount);
 
   return (
     <div className='cart-icon' onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{getItemNumber(cartItems)}</span>
+      <span className='item-count'>{cartItemsCount}</span>
     </div>
   )
 })
