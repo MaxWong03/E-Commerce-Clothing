9f135974133f0c328649a5876110747790f54a06
Max Wong <max@techsamurais.com>
2020-01-06 10:44:32 -0800

    include wanring message

--- src/pages/CheckOutPage/CheckOutPage.js
+++ src/pages/CheckOutPage/CheckOutPage.js
@@ -35,6 +35,11 @@ const CheckOutPage = () => {
       <div className='total'>
         <span>Total: ${cartTotal}</span>
       </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 -CVV: 123
+      </div>
       <StripeCheckoutButton price={cartTotal} />
     </div>
   )
