2b307c9d3b6379d72494d7b726d472fbbe1980e9
Max Wong <max@techsamurais.com>
2020-01-07 10:27:41 -0800

    remove unncessary file, convert directory to use styled component

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectDirectory } from '../../redux/directory/directory.selector';
-import './Directory.scss';
 import MenuItem from '../Menu-Item/MenuItem';
+import { DirectoryMenuContainer } from './Directory.styles';
 
 const Directory = () => {
   const sections = useSelector(selectDirectory);
   return (
-    <div className='directory-menu'>
+    <DirectoryMenuContainer>
       {
         sections.map(({ id, ...sectionProps }) => (
           <MenuItem key={id} {...sectionProps} />
         ))
       }
-    </div>
+    </DirectoryMenuContainer>
   );
 };
 
